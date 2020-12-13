799ed68b6dfa44dd1604bf90e3a339d687981d73
Dave <dwitt29@verizon.net>
2020-12-13 14:17:57 -0500

    adding $ to fix variables

--- production/modules/rpm_sudo/manifests/copyfile.pp
+++ production/modules/rpm_sudo/manifests/copyfile.pp
@@ -12,13 +12,13 @@ class rpm_sudo::copyfile {
   if $rpm_sudo::install_ensure == 'installed' {
     notify { "Copying suders": }
     file { $rpm_sudo::config_file:
-#      path	=> rpm_sudo::config_file,
-      ensure	=> rpm_sudo::config_file_ensure,
-      owner	=> rpm_sudo::config_file_owner,
-      group	=> rpm_sudo::config_file_group,
-      mode	=> rpm_sudo::config_file_mode,
-      backup	=> rpm_sudo::config_file_backup,
-      source	=> rpm_sudo::config_file_source,
+#      path	=> $rpm_sudo::config_file,
+      ensure	=> $rpm_sudo::config_file_ensure,
+      owner	=> $rpm_sudo::config_file_owner,
+      group	=> $rpm_sudo::config_file_group,
+      mode	=> $rpm_sudo::config_file_mode,
+      backup	=> $rpm_sudo::config_file_backup,
+      source	=> $rpm_sudo::config_file_source,
     }
   }
 
