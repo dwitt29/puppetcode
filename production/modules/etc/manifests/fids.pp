43cd0f9a7561d45ee2b5b85b112a67def3860623
Dave <dwitt29@verizon.net>
2020-12-17 15:27:38 -0500

    uninstall logic

--- production/modules/etc/manifests/fids.pp
+++ production/modules/etc/manifests/fids.pp
@@ -5,27 +5,30 @@
 # @example
 #   include etc::fids
 class etc::fids {
-    $hi = $facts.get('scruffy', 'no scruffy')
-    notify { "Checking \$os->\$family = ${facts['os']['family']}": }
-    notify { "Checking \$scruffy1 = ${facts['scruffy']}": }
-    notify { "Checking \$scruffy2 = $scruffy": }
-    notify { "Checking \$scruffy3 = $hi": }
-    if ( $facts['scruffy'] ) {
-        notify { "Scruffy is True": }
-    }     
+    #$hi = $facts.get('scruffy', 'no scruffy')
+    #notify { "Checking \$os->\$family = ${facts['os']['family']}": }
+    #notify { "Checking \$scruffy1 = ${facts['scruffy']}": }
+    #notify { "Checking \$scruffy2 = $scruffy": }
+    #notify { "Checking \$scruffy3 = $hi": }
+    #if ( $facts['scruffy'] ) {
+    #    notify { "Scruffy is True": }
+    #}     
 
     notify { "${facts[ 'rates' ]}": }
 
     if ( $facts[ 'rates' ] ) {
-        user { 'add Rates FID':
+        user { $etc::rates_user_name:
             ensure  =>  $etc::rates_user_install,
-            name    =>  $etc::rates_user_name,
             uid     =>  $etc::rates_user_uid,
             gid     =>  $etc::rates_user_gid,
             comment =>  $etc::rates_user_comment,            
             home    =>  $etc::rates_user_home,
             shell   =>  $etc::rates_user_shell,
             #groups  =>  $etc::rates_user_groups,
+        }
+    } else {
+        user { $etc::rates_user_name:
+            ensure  =>  $etc::rates_user_uninstall,
         }
     }
 }
