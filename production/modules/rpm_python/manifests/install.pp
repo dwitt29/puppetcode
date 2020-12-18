d7ef517ab58bb3e27bbbee2407186ec0fec21631
Dave <dwitt29@verizon.net>
2020-12-17 21:09:25 -0500

    add notify{} calls

--- production/modules/rpm_python/manifests/install.pp
+++ production/modules/rpm_python/manifests/install.pp
@@ -6,25 +6,26 @@
 #   include rpm_python::install
 class rpm_python::install {
 
-  if $rpm_python::install_python_ensure == 'purged' {
-
+  if $rpm_python::install_python_ensure == 'purged' {  # Removal
+    notify { $rpm_python::uninstall_python_message: }
     package { $rpm_python::uninstall_python_message:
       name    =>  $rpm_python::install_python_name,
       ensure  =>  $rpm_python::install_python_ensure,
     }
-
+    notify { $rpm_python::uninstall_pip_message: }
     package { $rpm_python::uninstall_pip_message:
       name    =>  $rpm_python::install_pip_name,
       ensure  =>  $rpm_python::install_pip_ensure,
     }
 
   } else {    # Install
-
+    notify { $rpm_python::install_python_message: }
     package { $rpm_python::install_python_message:
       name    =>  $rpm_python::install_python_name,
       ensure  =>  $rpm_python::install_python_ensure,
     }
-     package { $rpm_python::uninstall_pip_message:
+    notify { $rpm_python::install_pip_message: }
+    package { $rpm_python::uninstall_pip_message:
       name    =>  $rpm_python::install_pip_name,
       ensure  =>  $rpm_python::install_pip_ensure,
     }
