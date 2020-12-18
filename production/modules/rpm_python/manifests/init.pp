b7fbcbe48d87f41d5759b4ff7329b1bed8b89801
Dave <dwitt29@verizon.net>
2020-12-18 11:02:23 -0500

    add config for creating python and pip symlinks

--- production/modules/rpm_python/manifests/init.pp
+++ production/modules/rpm_python/manifests/init.pp
@@ -5,11 +5,19 @@ class rpm_python (
   String  $uninstall_python_message,
   String  $install_python_name,
   String  $install_python_ensure,
+  String  $rpm_python::install_python_linksrc,
+  String  $rpm_python::install_python_linkdst,
+  String  $rpm_python::install_python_link_ensure,
+  String  $rpm_python::uninstall_python_link_ensure,
 
   String  $install_pip_message,
   String  $uninstall_pip_message,
   String  $install_pip_name,
   String  $install_pip_ensure,
+  String  $rpm_python::install_pip_linksrc,
+  String  $rpm_python::install_pip_linkdst,
+  String  $rpm_python::install_pip_link_ensure,
+  String  $rpm_python::uninstall_pip_link_ensure,
   
   )
 {
--- production/modules/rpm_python/manifests/install.pp
+++ production/modules/rpm_python/manifests/install.pp
@@ -7,27 +7,53 @@
 class rpm_python::install {
 
   if $rpm_python::install_python_ensure == 'purged' {  # Removal
+
     notify { $rpm_python::uninstall_python_message: }
     package { $rpm_python::uninstall_python_message:
       name    =>  $rpm_python::install_python_name,
       ensure  =>  $rpm_python::install_python_ensure,
     }
+    
     notify { $rpm_python::uninstall_pip_message: }
     package { $rpm_python::uninstall_pip_message:
       name    =>  $rpm_python::install_pip_name,
       ensure  =>  $rpm_python::install_pip_ensure,
     }
 
-  } else {    # Install
+    notify { "Remove soft link $rpm_python::install_python_linkdst"}
+    file { $rpm_python::install_python_linkdst:
+      ensure    => $rpm_python::uninstall_python_link_ensure,
+    }
+
+    notify { "Remove soft link $rpm_python::install_pip_linkdst"}
+    file { $rpm_python::install_python_linkdst:
+      ensure    => $rpm_python::uninstall_pip_link_ensure,
+    }
+
+   } else {    # Install
+   
     notify { $rpm_python::install_python_message: }
     package { $rpm_python::install_python_message:
       name    =>  $rpm_python::install_python_name,
       ensure  =>  $rpm_python::install_python_ensure,
     }
+    
     notify { $rpm_python::install_pip_message: }
     package { $rpm_python::uninstall_pip_message:
       name    =>  $rpm_python::install_pip_name,
       ensure  =>  $rpm_python::install_pip_ensure,
+    }
+    
+    notify { "Create soft link $rpm_python::install_python_linkdst"}
+    file { $rpm_python::install_python_linkdst:
+      ensure    => $rpm_python::install_python_link_ensure,
+      target    => $rpm_python::install_python_linksrc,
+    }
+
+    notify { "Create soft link $rpm_python::install_pip_linkdst"}
+    file { $rpm_python::install_python_linkdst:
+      ensure    => $rpm_python::install_pip_link_ensure,
+      target    => $rpm_python::install_pip_linksrc,
     }
   }
 
